8812d4d1b63a000905afb0fda467e0521960e491
Helen <63614926+helen-uoft@users.noreply.github.com>
2025-04-13 20:06:29 -0400

    Create nrwrh.r

---
+++ nrwrh.r
@@ -0,0 +1,151 @@
+
+##require drug chemical similarity data as AD1, target sequence similarity data 
+##as AT1, and drug-target association data as B.
+combinedsimilarity <- function(B, AD1, AT1, gamad1, gamad2, gamat1, gamat2, m, n) {
+  # Initialize AD2 (drug-drug similarity based on common targets
+  AD2 <- diag(rowSums(t(B)))  # t(B) because drugs are columns
+  
+  for (i in 2:m) {
+    for (j in 1:(i - 1)) {
+      k <- sum(B[, i] == 1 & B[, j] == 1 & B[, i] == B[, j])
+      AD2[i, j] <- k
+    }
+  }
+  
+  for (i in 1:(m - 1)) {
+    for (j in (i + 1):m) {
+      AD2[i, j] <- AD2[j, i]
+    }
+  }
+  
+  # Initialize AT2 (target-target similarity based on common drugs)
+  AT2 <- diag(rowSums(B))
+  
+  for (i in 2:n) {
+    for (j in 1:(i - 1)) {
+      k <- sum(B[i, ] == 1 & B[j, ] == 1 & B[i, ] == B[j, ])
+      AT2[i, j] <- k
+    }
+  }
+  
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      AT2[i, j] <- AT2[j, i]
+    }
+  }
+  
+  diag(AT2)[diag(AT2) == 0] <- 1
+  diag(AD2)[diag(AD2) == 0] <- 1
+  
+  # Normalize AT2 and AD2
+  NAT2 <- matrix(0, n, n)
+  for (i in 1:n) {
+    for (j in 1:n) {
+      NAT2[i, j] <- AT2[i, j] / sqrt(AT2[i, i]) / sqrt(AT2[j, j])
+    }
+  }
+  
+  NAD2 <- matrix(0, m, m)
+  for (i in 1:m) {
+    for (j in 1:m) {
+      NAD2[i, j] <- AD2[i, j] / sqrt(AD2[i, i]) / sqrt(AD2[j, j])
+    }
+  }
+
+  # Combine similarities
+  AD <- (gamad1 *AD1 + gamad2 * NAD2) / (gamad1 + gamad2)
+  AT <- (gamat1 * AT1 + gamat2 * NAT2) / (gamat1 + gamat2)
+  
+  return(list(AD = AD, AT = AT))
+}
+
+
+#####this function was built for the prediction
+nrwrhdrugtarget <- function(backprobability, lanbuda, yita, gamad1, gamad2, gamat1, gamat2, drugID) {
+  # Load data
+  B<-as.matrix(read.table("code/example/interaction.txt"))
+  AT1 <- as.matrix(read.table("code/example/sequencesimilarity.txt"))
+  AD1 <- as.matrix(read.table("code/example/chemicalsimilarity.txt"))
+drug=as.data.frame(AD1)
+target=as.data.frame(AT1)
+adj=as.data.frame(B)
+  #HB: modify B by removing first column which is drug ID
+  n <- nrow(B)  # number of targets
+  m <- ncol(B)  # number of drugs
+  
+  #call previously built function to standardize drug and target own similarity matrix
+  sim_list <- combinedsimilarity(B, AD1, AT1, gamad1, gamad2, gamat1, gamat2, m, n)
+  AD <- sim_list$AD
+  AT <- sim_list$AT
+  
+  # Construct transition matrices
+  MTD <- matrix(0, n, m)
+  MDT <- matrix(0, m, n)
+  MT  <- matrix(0, n, n)
+  MD  <- matrix(0, m, m)
+  
+  for (i in 1:n) {
+    row_sum <- sum(B[i, ])
+    if (row_sum != 0) {
+      MTD[i, ] <- lanbuda * B[i, ] / row_sum
+    }
+  }
+  
+  for (i in 1:m) {
+    col_sum <- sum(B[, i])
+    if (col_sum != 0) {
+      MDT[i, ] <- lanbuda * B[, i] / col_sum
+    }
+  }
+  
+  for (i in 1:n) {
+    row_sum <- sum(AT[i, ])
+    if (sum(B[i, ]) == 0 && row_sum != 0) {
+      MT[i, ] <- AT[i, ] / row_sum
+    } else if (row_sum != 0) {
+      MT[i, ] <- (1 - lanbuda) * AT[i, ] / row_sum
+    }
+  }
+  
+  for (i in 1:m) {
+    row_sum <- sum(AD[i, ])
+    if (sum(B[, i]) == 0 && row_sum != 0) {
+      MD[i, ] <- AD[i, ] / row_sum
+    } else if (row_sum != 0) {
+      MD[i, ] <- (1 - lanbuda) * AD[i, ] / row_sum
+    }
+  }
+  
+  M1 <- cbind(MT, MTD)
+  M2 <- cbind(MDT, MD)
+  M  <- rbind(M1, M2)
+
+  # Initialize probabilities
+  u0 <- rep(1/length(u0),n)
+  
+  v0 <- rep(0, m)
+  v0[which(colnames(AD1)==drugID)] <- 1
+
+  p0 <- c((1 - yita) * u0, yita * v0)
+  p1 <- p0
+  p <- (1 - backprobability) * t(M) %*% p1 + backprobability * p0
+  
+  while (max(abs(p - p1)) > 1e-10) {
+    p1 <- p
+    p <- (1 - backprobability) * t(M) %*% p1 + backprobability * p0
+  }
+  
+  u <- p[1:n]
+
+  # Rank targets
+  ss <- as.data.frame(u)
+  dline<-data.frame(known=adj[,which(colnames(adj)==drugID)])
+  concate<-cbind(ss,dline,ind=seq(1:n))
+  comp <- concate[order(-concate$u), ]
+  
+  #first order among unknown associated target
+  first<-comp[which(comp$known==0)[1],]
+  top=row.names(adj[first$ind,])  
+  
+  return(top)
+}
